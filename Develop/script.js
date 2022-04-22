776f1dab56f7e254a0fe5c470d3771634442bb7b
macosta2015 <macosta2015@my.fit.edu>
2022-04-22 19:37:31 -0400

    commit

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,7 @@ console.log('hello world')
 
 const comments_section = document.getElementById("inlineFormInputGroupUsername");
 console.log(comments_section);
+console.log('After')
 
 // document.getElementById("inlineFormInputGroupUsername").innerHTML = 5 + 6;
 // var usernameInput = document.getElementById("inlineFormInputGroupUsername");
