7f2fe158baf5775514cc4a29bd4a635be0d0ceb2
macosta2015 <macosta2015@my.fit.edu>
2022-04-23 00:59:16 -0400

    commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 const myForm = document.getElementById("myForm");
 
 myForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("Form has been submitted!")
-  console.log(myForm.Username.value)
-  localStorage.setItem('8am', myForm.Username.value)
+    e.preventDefault();
+    console.log("Form has been submitted!")
+    console.log(myForm.Username.value)
+    localStorage.setItem('8am', myForm.Username.value)
 });
 
 
@@ -15,11 +15,38 @@ console.log('We are printing the value of name')
 console.log(localStorage.getItem('name'))
 
 
+//Code for editing the test that is already saved
+const storageInput = document.querySelector('.storage');
+const text = document.querySelector('.text');
+const button = document.querySelector('.button');
+const storedInput = localStorage.getItem('textinput');
+
+
+//This code is for savind the data 
+if(storageInput){
+    text.textContent = storedInput
+}
+
+storageInput.addEventListener('input', letter => {
+    text.textContent = letter.target.value
+})
+
+myForm.addEventListener('input', letter => {
+    text.textContent = letter.target.value
+})
+
+
+const saveToLocalStorage = () => {
+    localStorage.setItem('textinput', text.textContent)
+}
+button.addEventListener('click', saveToLocalStorage)
 
 // REFERENCE VIDEOS
-// 1. How to connect js with html by using form:
-// https://www.youtube.com/watch?v=I_fVO_NzT2g
+// LocalStorage for beginners (HTML and Javascript)
+// https://www.youtube.com/watch?v=rVyTjFofok0
 
+// How to connect js with html by using form:
+// https://www.youtube.com/watch?v=I_fVO_NzT2g
 
 // Sending the value 
 // https://www.youtube.com/watch?v=TUnfb944HFI
